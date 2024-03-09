023ca9288b07af8182f975b67e1226b0beb321e1
ram8919 <89965706+ram8919@users.noreply.github.com>
2024-03-09 12:03:52 +0530

    project.py

--- project.py
+++ project.py
@@ -6,7 +6,19 @@ import seaborn as sns
 %matplotlib inline
 
 
-data = pd.read_csv(".\input\Mental Health Dataset.csv")
+def grouped_bar_plot(data, group_cols, value_col):
+# Group the data by the specified columns
+    grouped_data = data.groupby(group_cols).size().unstack()
 
-data.groupby(['Country', 'Gender']).size().unstack().plot(kind='bar')
-data.groupby(['Country', 'Coping_Struggles']).size().unstack().plot(kind='bar')
+    # Plot the grouped data as a bar chart
+    grouped_data.plot(kind='bar')
+
+# import data
+data=pd.read_csv("/content/Mental Health Dataset.csv")
+
+# Plotting based on coping struggles
+grouped_bar_plot(data, ['Country', 'Coping_Struggles'], 'Count')
+
+# Plotting based on gender
+grouped_bar_plot(data, ['Country', 'Gender'], 'Count')
+print(grouped_bar_plot)
