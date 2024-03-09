f1af00fc07a2a2a558def6ced9ef554e3121d864
ram8919 <89965706+ram8919@users.noreply.github.com>
2024-03-09 11:44:20 +0530

    Update project.py
    Bar plot

--- project.py
+++ project.py
@@ -3,3 +3,6 @@ import pandas as pd
 
 
 data = pd.read_csv(".\input\Mental Health Dataset.csv")
+
+data.groupby(['Country', 'Gender']).size().unstack().plot(kind='bar')
+data.groupby(['Country', 'Coping_Struggles']).size().unstack().plot(kind='bar')
