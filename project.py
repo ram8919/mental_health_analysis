62d1096b798881fe6b502fd82db9d7278fe23709
ram8919 <89965706+ram8919@users.noreply.github.com>
2024-03-09 12:17:35 +0530

    project.py
    BAr

--- project.py
+++ project.py
@@ -2,6 +2,22 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from helper import *
 
-data = pd.read_csv(".\input\Mental Health Dataset.csv")
+data = pd.read_csv(".\input\Mental Health Dataset.csv")# Define a function to create grouped bar plots
+def grouped_bar_plot(data, group_cols, value_col):
+    
+    # Group the data by the specified columns
+    grouped_data = data.groupby(group_cols).size().unstack()
 
-print(bar)
+    # Plot the grouped data as a bar chart
+    grouped_data.plot(kind='bar')
+
+# Import DATA
+data=pd.read_csv("/content/Mental Health Dataset.csv")
+
+# Plotting based on coping struggles
+grouped_bar_plot(data, ['Country', 'Coping_Struggles'], 'Count')
+
+# Plotting based on gender
+grouped_bar_plot(data, ['Country', 'Gender'], 'Count')
+print(grouped_bar_plot)
+
