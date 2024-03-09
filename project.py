449a24dec4d3b64d3e79dd4c35ad14cf1c3262c8
ram8919 <89965706+ram8919@users.noreply.github.com>
2024-03-09 11:34:38 +0530

    project.py

--- project.py
+++ project.py
@@ -4,4 +4,7 @@ from helper import *
 
 data = pd.read_csv(".\input\Mental Health Dataset.csv")
 
+data.groupby(['Country', 'Gender']).size().unstack().plot(kind='bar')
+data.groupby(['Country', 'Coping_Struggles']).size().unstack().plot(kind='bar')
+
 print(bar)
