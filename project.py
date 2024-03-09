0dc1bcd2e402ed103997cb594706e6a35b468319
ram8919 <89965706+ram8919@users.noreply.github.com>
2024-03-09 11:30:34 +0530

    Update project.py

--- project.py
+++ project.py
@@ -1,7 +1,12 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-from helper import *
+import matplotlib.pyplot as plt
+import seaborn as sns
+%matplotlib inline
+
 
 data = pd.read_csv(".\input\Mental Health Dataset.csv")
 
-print(bar)
+df.groupby(['Country', 'Coping_Struggles']).size().unstack().plot(kind='bar')
+
+df.groupby(['Country', 'Gender']).size().unstack().plot(kind='bar')
